af64dcf3dfce847ee0021060738329e7354f8888
Gunjan-Gaur <gunjangaur4@gmail.com>
2020-01-21 18:01:17 +0530

    Max_of_three

---
+++ Ruby/Max_of_three.rb
@@ -0,0 +1,11 @@
+puts 'Max of 3 numbers'
+a=gets.chomp.to_i
+b=gets.chomp.to_i
+c=gets.chomp.to_i
+if a>b && a>c
+  puts 'a is greatest'
+elsif b>a && b>c
+  puts 'b is greatest'
+elsif c>a && c>b
+  puts 'c is greatest'  	
+end  		
--- Ruby/conditional_loop.rb
+++ Ruby/conditional_loop.rb
@@ -38,3 +38,17 @@ end
 names=['1','2','3']
 
 names.each{puts names}
+#---------------------------
+puts '--------------------------'
+puts 'Max of 3 numbers'
+a=gets.chomp.to_i
+b=gets.chomp.to_i
+c=gets.chomp.to_i
+if a>b && a>c
+  puts 'a is greatest'
+elsif b>a && b>c
+  puts 'b is greatest'
+elsif c>a && c>b
+  puts 'c is greatest'  	
+end  		  	
+  	  

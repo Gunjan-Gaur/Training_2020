6223e59d4f67793c7b0cfc61d3a7ce3bdb5c4fd8
Gunjan-Gaur <gunjangaur4@gmail.com>
2020-01-21 17:04:43 +0530

    until loop

--- Ruby/conditional_loop.rb
+++ Ruby/conditional_loop.rb
@@ -6,3 +6,19 @@ loop do
     break
   end  	
 end
+#---------------------
+x=gets.chomp.to_i
+while x>=0
+  puts x
+  x-=1
+end  
+puts "Complete"
+#-------------------
+x = gets.chomp.to_i
+
+until x < 0
+  puts x
+  x -= 1
+end
+
+puts "Complete"

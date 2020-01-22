85897edbfe1efd823212ec5eee0ea6413e41515a
Gunjan-Gaur <gunjangaur4@gmail.com>
2020-01-22 15:12:18 +0530

    Iterating over array

--- Ruby/Array.rb
+++ Ruby/Array.rb
@@ -78,3 +78,68 @@ Obtaining Information about the array
 2.7.0 :047 > arr.include?('c')
  => true 
 
+Adding elements to array
+---------------------------------------
+2.7.0 :051 > arr << 6
+ => ["a", "b", "c", "d", "c", "f", 6] 
+
+2.7.0 :052 > arr.unshift(0)
+ => [0, "a", "b", "c", "d", "c", "f", 6] 
+
+2.7.0 :053 > arr.insert(3,'Menu')
+ => [0, "a", "b", "Menu", "c", "d", "c", "f", 6] 
+
+2.7.0 :054 > arr.insert(3,'Menu','him')
+ => [0, "a", "b", "Menu", "him", "Menu", "c", "d", "c", "f", 6] 
+
+2.7.0 :055 > arr.push('l')
+ => [0, "a", "b", "Menu", "him", "Menu", "c", "d", "c", "f", 6, "l"] 
+
+Removing Itemns from an array
+----------------------------------------
+2.7.0 :071 > arr.pop
+ => 6 
+
+2.7.0 :064 > arr.push(nil)
+ => ["a", "b", "him", "Menu", "c", "d", "c", "f", nil] 
+
+2.7.0 :065 > arr.insert(3,nil)
+ => ["a", "b", "him", nil, "Menu", "c", "d", "c", "f", nil] 
+
+2.7.0 :066 > arr.compact
+ => ["a", "b", "him", "Menu", "c", "d", "c", "f"] 
+
+2.7.0 :067 > arr.insert(3,nil)
+ => ["a", "b", "him", nil, nil, "Menu", "c", "d", "c", "f", nil] 
+
+2.7.0 :068 > arr.compact!
+ => ["a", "b", "him", "Menu", "c", "d", "c", "f"] 
+
+2.7.0 :069 > arr.insert(3,'f','g')
+ => ["a", "b", "him", "f", "g", "Menu", "c", "d", "c", "f"] 
+
+2.7.0 :070 > arr.uniq
+ => ["a", "b", "him", "f", "g", "Menu", "c", "d"] 
+
+Iterating over array
+---------------------------------------
+2.7.0 :018 > fix = %w[mile jab hum tum]
+
+2.7.0 :019 > mix = ""
+
+2.7.0 :020 > fix.reverse_each {|make| mix += "#{make}"}
+
+2.7.0 :022 > p str
+ => " tumhumjabmile" 
+
+2.7.0 :028 > arr = [1,2,3,4,5,6,7,8,9]
+
+2.7.0 :029 > arr.map{|a| a**2 }
+ => [1, 4, 9, 16, 25, 36, 49, 64, 81] 
+
+2.7.0 :078 > arr.each{}
+ => [1, 2, 3, 4, 5, 6, 7, 8, 9] 
+
+2.7.0 :079 > arr.each{|a| print a -= 10, " " }
+-9 -8 -7 -6 -5 -4 -3 -2 -1  => [1, 2, 3, 4, 5, 6, 7, 8, 9] 
+ 

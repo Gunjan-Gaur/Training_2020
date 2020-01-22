a49642123490c0db1082a734bd17cc33e35475f3
Gunjan-Gaur <gunjangaur4@gmail.com>
2020-01-22 17:58:27 +0530

    Palindrome program

---
+++ Ruby/Palindrome.rb
@@ -0,0 +1,15 @@
+
+  num=123454321
+  rev=0
+  rem=0
+  original = num
+  while num != 0
+  	rem = num % 10
+  	rev = rev * 10 + rem
+  	num /= 10
+  end
+  if original==rev
+	puts "#{original} is palindrome"  	
+  else
+  	puts "#{original} is not palindrome"
+  end

4bd71c5fd615dc0f4e06d5f222021df47d5a54be
Gunjan-Gaur <gunjangaur4@gmail.com>
2020-01-27 13:08:33 +0530

    Operator Overloading

--- Ruby/method_overriding.rb
+++ Ruby/method_overriding.rb
@@ -22,3 +22,24 @@ end
 co = ChildArea.new(10,20)
 puts co.getArea	
 
+#------Operator Overloading---------------
+class Animal
+  attr_accessor:name,:color
+
+  def initialize(name,color)
+  	@name = name
+  	@color = color
+  end
+
+  def +(other_object)
+  	return Animal.new("#{self.name}#{other_object.name}","#{self.color}#{other_object.color}") 
+  end
+end
+
+class Zebra < Animal
+
+end	
+
+a = Zebra.new("bunny","black")
+b = Zebra.new("lily","gray")
+puts (a+b).inspect

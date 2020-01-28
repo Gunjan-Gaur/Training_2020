e32b137a9a6ec3719d29267ea3b976bae8a82681
Gunjan-Gaur <gunjangaur4@gmail.com>
2020-01-28 13:15:43 +0530

    Pick Random num from array,Amstrong num

--- Ruby/Array_Program.rb
+++ Ruby/Array_Program.rb
@@ -1,3 +1,4 @@
+puts "Dividing array according to even and odd:-"
 arr = ["1","2","3","4","5"]
 x = arr.length
 arr.each do
@@ -15,3 +16,33 @@ arr.each do
     break
   end	
 end	
+
+#Amstrong Number
+puts "Amstrong Number:-"
+a = gets.chomp.to_i
+temp=0,x=a,sum=0
+while x!=0
+  temp = x%10
+  sum = sum + (temp*temp*temp)
+  x /= 10
+end  
+
+if sum == a
+  puts "A"
+else
+  puts "NA"
+end
+
+#Sort according to length
+puts "Sort according to length:-"
+arr = ["hudhu","jidcnb","324","gyhu","der"]
+puts arr.sort_by(&:length)
+
+#Pick Random Number from array
+puts "Pick Random Number from array"
+arr1 = [1,7,90,45,42,76]
+puts arr1
+puts "2 random Number from array"
+puts arr1.sample(2)
+puts "3 random Number from array"
+puts arr1.sample(3)

86a05684ae8cb5711c81530e87a2c83356f98c26
Gunjan-Gaur <gunjangaur4@gmail.com>
2020-02-24 12:33:42 +0530

    Modified routes to include root department#patient

--- Project/Rails_Proj/Hospital_Management/config/routes.rb
+++ Project/Rails_Proj/Hospital_Management/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'department/patient' 
+  get 'department/patient'
+  root 'department#patient'
 end

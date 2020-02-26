9ff1fb776be67ad536fc6153373007ddd79b5790
Gunjan-Gaur <gunjangaur4@gmail.com>
2020-02-26 18:14:34 +0530

    Validation on password field in doctors javascript using rails form

--- Project/Rails_Proj/Hospital_Management/app/javascript/packs/doctor.js
+++ Project/Rails_Proj/Hospital_Management/app/javascript/packs/doctor.js
@@ -20,4 +20,17 @@ function myfun()
       m = mail.value
       m.match(/^[a-zA-Z0-9.]+@[a-zA-Z]+\.[a-zA-Z.]+$/)? mail.style.display="none" : mail.style.display="block";});
 
+      //Validation for password
+      pwd.addEventListener('focus',()=>{pass.style.display = "block";});
+
+      pwd.addEventListener('keyup',()=>{
+            p = pwd.value
+            p.match(/^[A-Z]+$/)?  p1.style.display="none" :p1.style.display="block";
+            p.match(/^[a-z]+$/)?  p2.style.display="none" :p2.style.display="block";
+            p.match(/^[0-9]+$/)?  p3.style.display="none" :p3.style.display="block";
+            p.match(/.{8}/)?      p4.style.display="none" :p4.style.display="block";});
+
+      pwd.addEventListener('focusout',()=>{pass.style.display = "none";});
+
+
 }

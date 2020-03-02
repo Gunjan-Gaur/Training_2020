97c51c60abb4862a8debe98bdf221b7e1b5c6b1c
Gunjan-Gaur <gunjangaur4@gmail.com>
2020-03-02 15:58:33 +0530

    Server side validation on doctor model

--- Project/Rails_Proj/Hospital_Management/app/models/doctor.rb
+++ Project/Rails_Proj/Hospital_Management/app/models/doctor.rb
@@ -3,4 +3,9 @@ class Doctor < ApplicationRecord
   has_many :patients , through: :appointments
   belongs_to :hospital
   has_many :medical_records
+  validates :First_name , :Last_name , format: {with: /\A[a-zA-Z]+\z/ ,message: 'Only alphabets are allowed'}, presence: {strict: true}
+  validates :salary ,numericality: true
+  validates :mobile ,numericality: {only_integer: true} ,length: {is: 10}
+  validates :email , format: {with: /\A[a-zA-Z0-9.]+@[a-zA-Z]+\.[a-zA-Z.]{3}+\z/ ,message: 'Format should match'}
+  validates :password , :confirm_password ,length: {is: 8}
 end

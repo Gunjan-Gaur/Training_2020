d2fb9be2e0969ab99f7d8d428f56ff3b4e202c99
Gunjan-Gaur <gunjangaur4@gmail.com>
2020-03-06 16:16:50 +0530

    Added Sorting in salary column doctors table

--- Project/Rails_Proj/Hospital_Management/app/controllers/doctors_controller.rb
+++ Project/Rails_Proj/Hospital_Management/app/controllers/doctors_controller.rb
@@ -1,5 +1,6 @@
 class DoctorsController < ApplicationController
   before_action :paginate
+  helper_method :sort_column, :sort_direction
   DOCTOR_PER_PAGE = 5
 
   def new
@@ -11,14 +12,14 @@ class DoctorsController < ApplicationController
     @page = params.fetch(:page,0).to_i
     # @page = params[:page] ? (params[:page].to_i) : 0
     if params[:search]
-      @doctors = Doctor.all.offset(@page*DOCTOR_PER_PAGE).limit(DOCTOR_PER_PAGE).order(:First_name).where(["email ilike ?","%#{params[:search]}%"])
+      @doctors = Doctor.all.offset(@page*DOCTOR_PER_PAGE).limit(DOCTOR_PER_PAGE).where(["email ilike ?","%#{params[:search]}%"]).order(sort_column + " " +sort_direction)
     else
-      @doctors = Doctor.offset(@page*DOCTOR_PER_PAGE).limit(DOCTOR_PER_PAGE).order(:First_name)
+      @doctors = Doctor.offset(@page*DOCTOR_PER_PAGE).limit(DOCTOR_PER_PAGE).order(sort_column + " " +sort_direction)
     end
   end
 
   def index
-    @doctors = Doctor.order(:First_name).limit(DOCTOR_PER_PAGE).offset(@page*DOCTOR_PER_PAGE)
+    @doctors = Doctor.limit(DOCTOR_PER_PAGE).offset(@page*DOCTOR_PER_PAGE).order(sort_column + " " +sort_direction)
   end
 
   def create
@@ -52,4 +53,12 @@ class DoctorsController < ApplicationController
     params.require(:doctor).permit(:First_name, :Last_name, :Qualifications, :Gender, :salary, :email , :mobile, :age , :password, :confirm_password, :comments ,:hospital_id,:avatar)
   end
 
+  private
+  def sort_column
+    params[:sort] || "salary"
+  end
+
+  def sort_direction
+    params[:direction] || "asc"
+  end
 end

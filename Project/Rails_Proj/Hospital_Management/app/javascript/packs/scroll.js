1bf86d0430f8c848728faf80975710ee302e7a37
Gunjan-Gaur <gunjangaur4@gmail.com>
2020-03-09 15:59:22 +0530

    Modified Pagination-Scrolling

--- Project/Rails_Proj/Hospital_Management/app/javascript/packs/scroll.js
+++ Project/Rails_Proj/Hospital_Management/app/javascript/packs/scroll.js
@@ -1,24 +1,16 @@
 
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded',() =>{
 
-  window.addEventListener('scroll',()=>{
+  window.addEventListener('scroll',() =>{
     console.log("1");
     let cval = 0
     let rowval = document.querySelectorAll('.rowval')
     cval = rowval.length
-    // $.ajax({
-    //   url: '/doctors',
-    //   type: 'get',
-    //   data: {
-    //      count: cval
-    //    },
-    //    dataType: "script",
-    // });
     $.ajax({
-          url:  "/doctors",
+          url:  "doctors/paginate",
           type: "get",
           data: {
-            count: cval,
+            count: cval
           },
           dataType: "script",
         });

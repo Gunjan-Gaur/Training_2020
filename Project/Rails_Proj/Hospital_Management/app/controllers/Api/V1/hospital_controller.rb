104eb6a04fcb605df869567d7e8719d658ff6558
Gunjan-Gaur <gunjangaur4@gmail.com>
2020-03-12 12:14:13 +0530

    Created Hospital Controller in Api/V1

---
+++ Project/Rails_Proj/Hospital_Management/app/controllers/Api/V1/hospital_controller.rb
@@ -0,0 +1,50 @@
+module Api
+  module V1
+    class HospitalController < CustomController
+      def index
+        @hospitals = Hospital.all
+        render json: @hospitals ,adapter: :json
+      end
+
+      def show
+        @hospital = Hospital.find_by(id: params[:id])
+        render json: @hospital ,adapter: :json
+      end
+
+      def create
+        @hospital = Hospital.new hospital_params
+        if @hospital.save
+          render json: @hospitals ,adapter: :json, status: 200
+        else
+          render json: {error: 'Unable to create hospital'}, status: 422
+        end
+      end
+
+      def update
+        @hospital = Hospital.find_by(id: params[:id])
+        if @hospital
+          @hospital.update hospital_params
+          render json: {message: 'Hospital successfully updated'}, status: 200
+        else
+          render json: {error: 'Unable to Update'}, status: 422
+        end
+      end
+
+      def destroy
+        @hospital = Hospital.find_by(id: params[:id])
+        if @hospital
+          @hospital.destroy
+          render json: {message: 'Successfully destroyed'}, status: 200
+        else
+          render json: {error: 'Not destroyed'}, status: 400
+        end
+      end
+
+      private
+      def hospital_params
+        params.permit(:Name , :Address , :State , :Beds)
+      end
+
+    end
+  end
+end

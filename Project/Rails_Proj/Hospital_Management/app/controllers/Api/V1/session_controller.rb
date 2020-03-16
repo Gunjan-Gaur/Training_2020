44a0f02e73fe3c4b37a8297d5d329748a56dc4d5
Gunjan-Gaur <gunjangaur4@gmail.com>
2020-03-16 17:31:45 +0530

    Updated session_controller

--- Project/Rails_Proj/Hospital_Management/app/controllers/Api/V1/session_controller.rb
+++ Project/Rails_Proj/Hospital_Management/app/controllers/Api/V1/session_controller.rb
@@ -3,16 +3,16 @@ module Api
     class SessionController < CustomController
       def login
         @doctor = Doctor.find_by(email: params[:email])
-        @id = Doctor.find_by(id: params[:id])
+       # @id = Doctor.find_by(id: params[:id])
         if @doctor and @doctor.password == params[:password]
-          jwt = JWT.encode(
+          token = JWT.encode(
             {doctor_id: @doctor.id, exp: (Time.now + 2.weeks).to_i},
             Rails.application.secrets.secret_key_base,
             'HS256'
           )
-        	render json: {token: jwt, message: "successfully passed" }
+        	render json: {token: token, message: "successfully passed" }, status: :ok
         else
-        	render json: {message: "Not successfull"}
+        	render json: {message: "Not successfull"}, status: :unauthorized
         end
       end
     end

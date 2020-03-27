8bf413fc505afe67e135558c3674c2103135de6e
Gunjan-Gaur <gunjangaur04@gmail.com>
2020-03-27 20:06:45 +0530

    Added new link and show details in database

--- Project/Rails_Proj/Hospital_Management/app/controllers/appointments_controller.rb
+++ Project/Rails_Proj/Hospital_Management/app/controllers/appointments_controller.rb
@@ -1,9 +1,23 @@
 class AppointmentsController < ApplicationController
   def new
-      @appointments = Appointment.new
+    @appointments = Appointment.new
   end
 
   def index
-      @appointments = Appointment.all.order(created_at: :desc)
+    @appointments = Appointment.all.order(created_at: :desc)
+  end
+
+  def show
+    @appointments = Appointment.find_by(id: params[:id])
+  end
+
+  def create
+    appointments = Appointment.new appointment_params
+    appointments.save
+    redirect_to appointments_path
+  end
+
+  def appointment_params
+    params.require(:appointments).permit(:Start_date,:End_date,:doctor_id,:patient_id)
   end
 end

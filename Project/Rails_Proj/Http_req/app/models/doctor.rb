6758874f1d57f0938dce0aead1ee04ca4b26c3e5
Gunjan-Gaur <gunjangaur04@gmail.com>
2020-03-30 18:21:24 +0530

    Added controller in http_req app

--- Project/Rails_Proj/Http_req/app/models/doctor.rb
+++ Project/Rails_Proj/Http_req/app/models/doctor.rb
@@ -1,11 +1,10 @@
+
 class Doctor < ApplicationRecord
   include HTTParty
   Doctor.get('http://localhost:3000/api/v1/doctors')
   # default_params fields: "doctor_id,first_name", q:"search"
   format :json
-
   # def self.for term
   #   get("",query: { query: term})["doctors"]
   # end
-
 end

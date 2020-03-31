99452878b25e769c201e3911e7ab0a6957df079b
Gunjan-Gaur <gunjangaur04@gmail.com>
2020-03-31 17:27:54 +0530

    Modified controller,model in http_req

--- Project/Rails_Proj/Http_req/app/controllers/doctors_controller.rb
+++ Project/Rails_Proj/Http_req/app/controllers/doctors_controller.rb
@@ -1,6 +1,7 @@
 class DoctorsController < ApplicationController
   def index
-    # @search_term = 'Nobita'
-    @doc = Doctor.all
+     # @search_term = 'Nobita'
+     # @doc = Doctor.for (@search_term)
+     @doctor = HTTParty.get('http://localhost:3000/api/v1/doctors', :headers => {'content.type'=>'application/json'})
   end
 end

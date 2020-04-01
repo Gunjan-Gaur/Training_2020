92e094995874b7fd9464237072fe3f30fe051ffe
Gunjan-Gaur <gunjangaur04@gmail.com>
2020-04-01 17:47:03 +0530

    Added create action in doctors_controller

--- Project/Rails_Proj/Http_req/app/controllers/doctors_controller.rb
+++ Project/Rails_Proj/Http_req/app/controllers/doctors_controller.rb
@@ -1,8 +1,11 @@
 class DoctorsController < ApplicationController
   def index
-     # @search_term = 'Nobita'
-     # @doc = Doctor.for (@search_term)
-     @doctor = HTTParty.get('http://localhost:3001/api/v1/doctors', :headers => {'content.type'=>'application/json'})
-     # debugger
+    @doctor = HTTParty.get('http://localhost:3001/api/v1/doctors', :headers => {'content.type'=>'application/json'})
+  end
+  def new
+    @doctor = Doctor.new
+  end
+  def create
+    @doctor = HTTParty.get('http://localhost:3001/api/v1/doctors?first_name=&last_name=&qualifications=&gender=&salary=&email=&mobile=&age=&password=&confirm_password=&comments=&hospital_id=', :headers => {'content.type'=>'application/json'})
   end
 end

bcb162f30066d0556bac51dcc5a5017d9ceb4365
Gunjan-Gaur <gunjangaur04@gmail.com>
2020-04-01 17:47:42 +0530

    Added javascript on textfields in doctor.js file

---
+++ Project/Rails_Proj/Http_req/app/javascript/packs/doctor.js
@@ -0,0 +1,90 @@
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("1");
+    const password = document.querySelector('#password');
+    const mixid = document.querySelector('#mixid');
+    const p1 = document.querySelector('#p1');
+    const p2 = document.querySelector('#p2');
+    const p3 = document.querySelector('#p3');
+    const p4 = document.querySelector('#p4');
+    const confirm_pwd = document.querySelector('#confirm_pwd');
+
+    //password validation
+     password.addEventListener('focus',()=>{mixid.style.display = "block";});
+     password.addEventListener('focusout',()=>{mixid.style.display = "none";});
+    password.addEventListener('keyup',()=>{
+      val = password.value
+      val.match(/[A-Z]+/)? p1.style.display="none" :p1.style.display="block"
+      val.match(/[a-z]+/)? p2.style.display="none" :p2.style.display="block"
+      val.match(/[0-9]+/)? p3.style.display="none" :p3.style.display="block"
+      val.match(/.{8}/)? p4.style.display="none" :p4.style.display="block"
+    });
+    // confirm_pwd.addEventListener('focus',()=>{p5.style.display="block";});
+    // confirm_pwd.addEventListener('focusout',()=>{p5.style.display="none";});
+    confirm_pwd.addEventListener('keyup',()=>{
+      x = password.value
+      password.value == confirm_pwd.value? p5.style.display="none":p5.style.display="block";
+    });
+    console.log("2");
+    //Validate firstname
+    const firstname = document.querySelector('#firstname');
+    const fname = document.querySelector('#fname');
+    //fname.addEventListener('focus',()=>{firstname.style.display = "block";});
+    //fname.addEventListener('focusout',()=>{firstname.style.display = "none";});
+    fname.addEventListener('keyup',()=>{
+      v = fname.value
+      v.match(/^[a-zA-Z]+$/) && !v == " "? firstname.style.display="none":firstname.style.display="block"});
+
+    //Validate lastname
+    const lastname = document.querySelector('#lastname');
+    const lname = document.querySelector('#lname');
+    //lname.addEventListener('focus',()=>{firstname.style.display = "block";});
+    //lname.addEventListener('focusout',()=>{firstname.style.display = "none";});
+    lname.addEventListener('keyup',()=>{
+      v = lname.value
+      v.match(/^[a-zA-Z]+$/)? lastname.style.display="none":lastname.style.display="block"});
+
+      console.log("3");
+      //validate email
+      const email = document.querySelector('#email');
+      const mail = document.querySelector('#mail');
+      // email.addEventListener('focus',()=>{mail.style.display='block';})
+      // email.addEventListener('focusout',()=>{mail.style.display='none';})
+      email.addEventListener('keyup',()=>{
+      v = email.value
+      v.match(/^[a-zA-Z0-9.]+@[a-zA-Z]+\.[a-zA-Z.]{3}$/)? mail.style.display='none':mail.style.display='block';
+      });
+      console.log("4");
+      //validate phone Number
+      const mobile = document.querySelector('#mobile');
+      const phone = document.querySelector('#phone');
+      // mobile.addEventListener('focus',()=>{phone.style.display='block';})
+      // mobile.addEventListener('focusout',()=>{phone.style.display='none';})
+      mobile.addEventListener('keyup',()=>{
+        v = mobile.value
+        isNaN(v)? phone.style.display='block':phone.style.display='none'
+      });
+
+
+       const all_inputs = document.querySelectorAll('.input');
+       const myform = document.querySelector('#myform');
+       console.log("5");
+       myform.addEventListener('submit', function make(e) {
+         console.log("6");
+         count=0;
+         for(var i=0;i<all_inputs.length;i++)
+         {
+           if(all_inputs[i].value =='')
+           {
+             console.log("hey");
+             count++;
+             console.log(count);
+           }
+         }
+         if(count>0)
+         {
+           alert("Please enter all fields");
+           e.preventDefault();
+         }
+      });
+});
